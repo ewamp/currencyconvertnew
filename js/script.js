75a4d2169284bbe4ae00529179a6d9f71b732ae1
Ewa Madej-Pawlowska <ewa.madej@gmail.com>
2025-05-16 10:44:33 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let resultElement = document.querySelector(".js-result");
+let currencyElement = document.querySelector(".js-currency");
+
+let rateUSD = 3.7775;
+let rateGBP = 5.0419;
+let rateEUR = 4.2455;
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "USD":
+            result = amount / rateUSD;
+            break;
+
+        case "GBP":
+            result = amount / rateGBP;
+            break;
+
+        case "EUR":
+            result = amount / rateEUR;
+    }
+
+    resultElement.innerHTML = `${amount} PLN = <strong>${result.toFixed(2)} ${currency}
+    </strong>`;
+
+});
